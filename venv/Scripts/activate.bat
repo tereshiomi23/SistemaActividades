1ecc4b8ddd8880fc0039b6fad8e6b8301cc3e1a4
tereshiomi23 <josmaryteresapulgars@gmail.com>
2026-04-28 01:26:01 -0400

    css consultar

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jtpulgar\Desktop\SistemaActividades\venv
+set VIRTUAL_ENV=C:\Users\teres\SistemaActividades\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
